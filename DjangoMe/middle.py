730c1215398b6bc9895189298968d5db542153ac
Edwin <edwin.eames214@gmail.com>
2015-05-04 16:55:32 -0400

    middleware/track shares

---
+++ DjangoMe/middle.py
@@ -0,0 +1,16 @@
+__author__ = 'ed'
+from joins.models import Join
+
+
+class referMiddleware():
+    def process_request(self, request):
+            ref_id = request.GET.get("ref")
+            print(ref_id)
+            try:
+                obj = Join.objects.get(ref_id = ref_id)
+            except:
+                obj = None
+
+            if obj:
+                request.session['join_id_ref'] = obj.id
+
